7d5fedcbe1d21ce86a496395faa93275dd40620a
Nik <nikolas.sapalidis@gmail.com>
2026-02-21 21:40:15 +0200

    fixes 32

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ─── CORS ───
